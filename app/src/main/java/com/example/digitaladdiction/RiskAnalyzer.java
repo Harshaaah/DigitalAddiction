bb14a83c5f8c29411104a07ba0723b5c02f0073f
Harsha P <harsharam2020@gmail.com>
2026-01-16 10:00:00 +0530

    risk analyzer

--- app/src/main/java/com/example/digitaladdiction/RiskAnalyzer.java
+++ app/src/main/java/com/example/digitaladdiction/RiskAnalyzer.java
@@ -1,29 +1,29 @@
-//package com.example.digitaladdiction;
-//
-//import java.util.Calendar;
-//
-//public class RiskAnalyzer {
-//
-//    public enum RiskLevel {
-//        LOW, MODERATE, HIGH, SEVERE
-//    }
-//
-//    // Returns risk based on milliseconds played
-//    public static RiskLevel calculateRisk(long totalUsageMs) {
-//        long hours = totalUsageMs / (1000 * 60 * 60);
-//
-//        if (hours < 2) return RiskLevel.LOW;
-//        if (hours < 4) return RiskLevel.MODERATE;
-//        if (hours < 6) return RiskLevel.HIGH;
-//        return RiskLevel.SEVERE;
-//    }
+package com.example.digitaladdiction;
 
-    // Checks if current time is between 11 PM and 5 AM
-//    public static boolean isLateNight() {
-//        Calendar calendar = Calendar.getInstance();
-//        int hour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
-//
-//        // Late night is 23 (11PM) to 5 (5AM)
-//        return (hour >= 23 || hour < 5);
-//    }
+import java.util.Calendar;
+
+public class RiskAnalyzer {
+
+    public enum RiskLevel {
+        LOW, MODERATE, HIGH, SEVERE
+    }
+
+    // Returns risk based on milliseconds played
+    public static RiskLevel calculateRisk(long totalUsageMs) {
+        long hours = totalUsageMs / (1000 * 60 * 60);
+
+        if (hours < 2) return RiskLevel.LOW;
+        if (hours < 4) return RiskLevel.MODERATE;
+        if (hours < 6) return RiskLevel.HIGH;
+        return RiskLevel.SEVERE;
+    }
+
+     //Checks if current time is between 11 PM and 5 AM
+    public static boolean isLateNight() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
+
+        // Late night is 23 (11PM) to 5 (5AM)
+        return (hour >= 23 || hour < 5);
+    }
 }
